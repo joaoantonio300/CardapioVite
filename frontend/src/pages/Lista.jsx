c78714982bdbbec99742d91e8d5c3d5be0d39b36
joao <joaoantoniosantosalves1@gmail.com>
2025-08-05 14:41:01 -0300

    feat: puxando elementos

--- frontend/src/pages/Lista.jsx
+++ frontend/src/pages/Lista.jsx
@@ -2,25 +2,19 @@ import Drink from "../assets/drink.png";
 import Plate from "../assets/plate.png";
 import Layout from "../Layout/Layout";
 import NavBar from "../Components/NavBar";
+import {useNavigate, Link} from "react-router-dom";
+import { useState } from 'react';
+import { useFetchDocuments } from '../hooks/useFetchDocuments';
 
 const title="Olá, Bem vindo";
 const subtitle="Escolha seu pedido entre bebidas, lanches e combos!"
-const items = [
-  { img: Drink, title: "Refrigerante", desc: "Sabor cola", price: "01.00" },
-  { img: Plate, title: "Prato", desc: "Com batatas", price: "05.00" },
-  { img: Drink, title: "Refrigerante", desc: "Sabor cola", price: "01.00" },
-  { img: Plate, title: "Prato", desc: "Com batatas", price: "05.00" },
-  { img: Drink, title: "Refrigerante", desc: "Sabor cola", price: "01.00" },
-  { img: Plate, title: "Prato", desc: "Com batatas", price: "05.00" },
-  { img: Drink, title: "Refrigerante", desc: "Sabor cola", price: "01.00" },
-  { img: Plate, title: "Prato", desc: "Com batatas", price: "05.00" },
-  { img: Drink, title: "Refrigerante", desc: "Sabor cola", price: "01.00" },
-  { img: Plate, title: "Prato", desc: "Com batatas", price: "05.00" },
-  { img: Drink, title: "Refrigerante", desc: "Sabor cola", price: "01.00" },
-  { img: Plate, title: "Prato", desc: "Com batatas", price: "05.00" },
-];
 
 const Lista = () => {
+
+
+  const [query, setQuery] = useState("");
+  const {documents: items, loading, error} = useFetchDocuments("posts");
+
   return (
     <>
       <Layout
@@ -35,14 +29,13 @@ const Lista = () => {
             <li>Lanches</li>
             <li>Combos</li>
           </ul>
-
           <div className="flex items-center justify-center">
             <div className="flex-wrap mt-10 w-[95%] flex flex-row justify-between">
             {items && items.length > 0 ? (items.map((item, i) => (
                <div key={i} className="flex flex-col rounded-[20%] justify-center items-center shadow-md w-[32%]">
-               <img className="h-[7em]" src={item.img} alt="" />
-               <h3 className="font-poppins text-[10px]">{item.title}</h3>
-               <p className="text-[7px]">{item.desc}</p>
+               <img className="h-[7em]"  src={`http://localhost:5000${item.imageUrl}`} alt="" />
+               <h3 className="font-poppins text-[10px]">{item.name}</h3>
+               <p className="text-[7px]">{item.description}</p>
                <p className="text-[8px] mr-12">$ {item.price}</p>
              </div>
             ))) : (
